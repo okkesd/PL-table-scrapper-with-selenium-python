21f4d21d02030728fe13a0e366ef4e8c5bbcf280
okkesd <okkesdonbaloglu25@gmail.com>
2024-03-06 23:06:53 +0300

    uptaded files

--- data.py
+++
@@ -1,3 +0,0 @@
-dictionary = {"raj": 2, "striver": 3, "vikram": 4}
-arr = list(dictionary.keys())
-print(arr)
--- denmeemem.py
+++
@@ -1,6 +0,0 @@
-i=1
-value = "//tbody/tr[" + str(i) + "]/td"
-while i<26:
-    value = "//tbody/tr[" + str(i) + "]/td"
-    print(value)
-    i = i+1
